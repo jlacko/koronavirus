fd1991d062e1172ba4d68e902b584b82c6956ae6
jlacko <jindra.lacko@gmail.com>
2020-03-21 13:38:25 +0000

    orchestrace mimo git

--- script/9-orchestrator.R
+++
@@ -1,6 +0,0 @@
-# pouze pro zjednodušení orchestrace; vnitřní logika žádná
-  
-  source('./script/1-digest-data.R') # stahne surová data a uloží do adresáře /data
-  source('./script/2-draw-chart.R')    # z dat v /data spočte obrázek o ČR a trendu
-  source('./script/3-srovnani-zemi.R') # z dat v /data spočte obrázek o sronvání se světem
-
