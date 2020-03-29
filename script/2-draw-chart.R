ad28ee5f7a86030e35ea2c0345441cfb7e1b94ce
Jindra Lacko <jindra.lacko@gmail.com>
2020-03-29 22:12:46 +0200

    doubling time starého a nového trendu

--- script/2-draw-chart.R
+++ script/2-draw-chart.R
@@ -8,17 +8,35 @@ clean_data <- read_csv2("./data/raw_data.csv") %>%
   filter(zeme == "Czechia" & pocet > 0) %>%
   mutate(den = lubridate::day(datum))
 
-trend <- nls(pocet ~ a * (1 + r)^(den),
-  data = subset(clean_data, den < 21),
+
+# stará trend line - do 20. 3. včetně
+old_trend <- nls(pocet ~ a * (1 + r)^(den),
+  data = subset(clean_data, den <= 20),
   start = list(a = 1, r = .01)
 )
 
+# čas ve dnech pro zdvojnásobení počtu nakažených; starý trend
+double_old <- log(2) / log(1 + coef(old_trend)[["r"]])
+
+# predikce budoucnosti podle starého trendu
 budoucnost <- data.frame(den = 21:31)
 
 predpoved <- data.frame(
   datum = c(as.Date("2020-03-21"):as.Date("2020-03-31")) %>% as.Date(origin = as.Date("1970-01-01")),
-  pocet = predict(trend, newdata = budoucnost)
+  pocet = predict(old_trend, newdata = budoucnost)
 )
+
+# nová trend line - od 21. 3. dále
+new_trend <- nls(pocet ~ a * (1 + r)^(den),
+  data = subset(clean_data, den > 20) %>% mutate(den = den - 19),
+  start = list(a = 1, r = .01)
+)
+
+# čas ve dnech pro zdvojnásobení počtu nakažených; nový trend
+double_new <- log(2) / log(1 + coef(new_trend)[["r"]])
+
+popisek_old <- paste("Trend před 20. březnem – zdvojnásobení počtu nakažených 1× za", str_replace(round(double_old, 2), "\\.", ","), "dní")
+popisek_new <- paste("Trend po 20. březnu – zdvojnásobení počtu nakažených 1× za", str_replace(round(double_new, 2), "\\.", ","), "dní")
 
 ggplot(data = clean_data, aes(x = datum, y = pocet)) +
   geom_text(
@@ -42,6 +60,8 @@ ggplot(data = clean_data, aes(x = datum, y = pocet)) +
     x = datum, y = pocet,
     label = formatC(pocet, big.mark = " ", format = "f", digits = 0)
   ), hjust = -.5, color = "firebrick") +
+  annotate("text", label = popisek_old, x = as.Date("2020-03-12"), y = 7, hjust = 0) +
+  annotate("text", label = popisek_new, x = as.Date("2020-03-12"), y = 4.5, hjust = 0) +
   labs(
     title = "Trend šíření nákazy COVID-19 v ČR",
     color = "Počet nakažených",
@@ -56,7 +76,7 @@ ggplot(data = clean_data, aes(x = datum, y = pocet)) +
   ) +
   scale_y_log10(labels = scales::number_format()) +
   scale_color_identity(
-    labels = c("skutečnost", "trend od 20.3.2020"),
+    labels = c("skutečnost", "trend po 20.3.2020"),
     guide = guide_legend(
       title.position = "top",
       title.hjust = 0.5
